547b09bcd4260310c6ef955b86b6a9e2b804e4fe
Jess Harrell <jharrell@pillartechnology.com>
2018-06-15 07:48:55 -0500

    put tests back

--- scripts/run_ci_docker.js
+++ scripts/run_ci_docker.js
@@ -6,20 +6,19 @@ npmInstall.stderr.on('data', function(data) {
     console.log(data.toString());
 });
 npmInstall.on('close', (exitCode) => {
-    // console.log("Install complete, starting build");
-    // const build = spawn('npm', ['build']);
-    // build.on('close', () => {
-    //     console.log("Build complete, starting tests");
-    //     const test = spawn('npm', ['run', 'ci']);
-    //     test.stdout.on('data', function(data) {
-    //         console.log(data.toString());
-    //     });
-    //     test.stderr.on('data', function(data) {
-    //         console.log(data.toString());
-    //     });
-    //     test.on('close', (exitCode) => {
-    //         process.exit(exitCode);
-    //     });
-    // });
-    process.exit(exitCode);
+    console.log("Install complete, starting build");
+    const build = spawn('npm', ['build']);
+    build.on('close', () => {
+        console.log("Build complete, starting tests");
+        const test = spawn('npm', ['run', 'ci']);
+        test.stdout.on('data', function(data) {
+            console.log(data.toString());
+        });
+        test.stderr.on('data', function(data) {
+            console.log(data.toString());
+        });
+        test.on('close', (exitCode) => {
+            process.exit(exitCode);
+        });
+    });
 });
